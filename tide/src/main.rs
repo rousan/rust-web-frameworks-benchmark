1f4aa7f8768e7d4b331b56329b90355f38530f23
Vaibhav <vaibhz@gmail.com>
2022-01-30 12:35:15 +0000

    Upgraded all libraries to latest version and added a new framework, tide

---
+++ tide/src/main.rs
@@ -0,0 +1,7 @@
+#[async_std::main]
+async fn main() -> Result<(), std::io::Error> {
+    let mut app = tide::new();
+    app.at("/").get(|_| async { Ok("Hello, world!") });
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
